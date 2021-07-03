0cc2307a412467dedc000bd617728066ed342328
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-07-04 01:04:19 +0300

    overide methode

--- app/src/main/java/com/moringaschool/g_samaritan/LostActivity.java
+++ app/src/main/java/com/moringaschool/g_samaritan/LostActivity.java
@@ -1,15 +1,42 @@
 package com.moringaschool.g_samaritan;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class LostActivity extends AppCompatActivity {
-    private Button mlogInbutton;
+public class LostActivity extends  AppCompatActivity {
+    TextView textView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lost);
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_lost);
+//        Intent intent =getIntent();
+//        String name= intent.getStringExtra("name");
+//        String country =intent.getStringExtra("country");
+//        textView=findViewById(R.id.findViewById);
+//        textView.setText("Welcome back" + name + "name" );
+//    }
 }
+
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.widget.TextView;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//class RestaurantsActivity extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_lost);
+//        TextView mEdittext = (TextView) findViewById(R.id.edittext);
+//        Intent intent = getIntent();
+//        String name = intent.getStringExtra("name");
+//
+//        mEdittext.setText("Welcome back:" + name);
+//    }
+//}
--- app/src/main/java/com/moringaschool/g_samaritan/MainActivity.java
+++ app/src/main/java/com/moringaschool/g_samaritan/MainActivity.java
@@ -1,5 +1,13 @@
 package com.moringaschool.g_samaritan;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,14 +23,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        name =findViewById(R.id.editText);
-        county=findViewById(R.id.edittext);
+        name =findViewById(R.id.findViewById);
+        county=findViewById(R.id.findViewById);
     }
 //
     public void switchActivity(View view){
-        Intent intent = new Intent(this.lostActivityclass)
-        intent.putExtra("user",name.getText());
-        intent.putExtra("country",county.getText());
+        Intent intent = new Intent(this,LostActivity.class);
+        intent.putExtra("name",name.getText() .toString()) ;
+        intent.putExtra("country",county.getText() .toString());
         startActivity(intent);
     }
 }
+//public class MainActivity extends AppCompatActivity {
+//    public static final String TAG = MainActivity.class.getSimpleName();
+//
+//    private Button mlogInbutton;
+//    private EditText mEdittext;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        mEdittext = (EditText) findViewById(R.id.edittext);
+//        mlogInbutton = (Button) findViewById(R.id.mlogInbutton);
+//        mlogInbutton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//                public void onClick(View v) {
+//                    String edittex = mEdittext.getText().toString();
+//                    Intent intent = new Intent(MainActivity.this, lostActivity.class);
+//                    startActivity(intent);
+//                }
+//
+//
+//
+//        });
+//    }
+//}
+
+

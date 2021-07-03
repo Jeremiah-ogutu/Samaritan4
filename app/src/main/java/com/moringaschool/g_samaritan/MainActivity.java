b6c0bb1e097e28ff73578efc3a3fbe38aeaee50a
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-07-03 17:16:23 +0300

    Activity

--- app/src/main/java/com/moringaschool/g_samaritan/MainActivity.java
+++ app/src/main/java/com/moringaschool/g_samaritan/MainActivity.java
@@ -2,13 +2,27 @@ package com.moringaschool.g_samaritan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText name;
+    EditText county;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        name =findViewById(R.id.editText);
+//        county=findViewById(R.id.edittext);
+//    }
+//
+//    public void switchActivity(View view){
+//        Intent intent = new Intent(this.lostActivityclass)
+//        intent.putExtra("user",name.getText());
+//        intent.putExtra("country",county.getText());
+//        startActivity(intent);
+//    }
+//}

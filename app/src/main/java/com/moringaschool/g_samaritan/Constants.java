4626ea657a95cdd095b7ed68339ff3474386aa7f
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-07-09 19:11:40 +0300

    add interface

---
+++ app/src/main/java/com/moringaschool/g_samaritan/Constants.java
@@ -0,0 +1,5 @@
+package com.moringaschool.g_samaritan;
+
+public class Constants {
+    public static  final  String  G_SAMARITAN_BASE_URL = "http://universities.hipolabs.com/";
+}
---
+++ app/src/main/java/com/moringaschool/g_samaritan/network/SamaritanApi.java
@@ -0,0 +1,4 @@
+package com.moringaschool.g_samaritan.network;
+
+public interface SamaritanApi {
+}

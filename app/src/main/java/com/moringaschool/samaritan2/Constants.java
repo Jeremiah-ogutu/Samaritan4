d0821cc5a9d820986b7dbf03b904eae2dcea1bc2
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-07-10 15:24:27 +0300

    initial commit

---
+++ app/src/main/java/com/moringaschool/samaritan2/Constants.java
@@ -0,0 +1,5 @@
+package com.moringaschool.samaritan2;
+
+public class Constants {
+    public static  final  String  G_SAMARITAN_BASE_URL = "http://universities.hipolabs.com/";
+}
---
+++ app/src/main/java/com/moringaschool/samaritan2/SamaritanClient.java
@@ -0,0 +1,29 @@
+package com.moringaschool.samaritan2;
+
+import com.moringaschool.samaritan2.Constants;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class SamaritanClient {
+
+    private static Retrofit retrofit = null;
+
+    public static SamaritanApi getClient() {
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()//reate new retrofit instance
+                    .baseUrl(Constants.G_SAMARITAN_BASE_URL)// Api base url
+//                  .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())// converter
+                    .build();
+        }
+        return  retrofit.create(SamaritanApi.class);
+
+    }
+}

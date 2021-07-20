f35ffacf6185ad8bfd1ae8ed3bc20790469578e7
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-07-20 16:34:16 +0300

    New activity

---
+++ app/src/main/java/com/moringaschool/g_samaritan/SavedUniversitiesListActivity.java
@@ -0,0 +1,13 @@
+package com.moringaschool.g_samaritan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SavedUniversitiesListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

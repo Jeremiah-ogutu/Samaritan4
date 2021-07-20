dc027283d53d08098aefb17653948f1d2cccd783
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-07-20 18:57:37 +0300

    main-Activity

--- app/src/main/java/com/moringaschool/g_samaritan/MainActivity.java
+++ app/src/main/java/com/moringaschool/g_samaritan/MainActivity.java
@@ -2,10 +2,12 @@ package com.moringaschool.g_samaritan;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,18 +17,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private ValueEventListener mSearchedCountryReferenceListener;
 
     private DatabaseReference mSearchedCountryReference;
 
+
+
     @BindView(R.id.findViewById) EditText name ;
     @BindView(R.id.EditText) EditText county;
+    @BindView(R.id.savedUniversitiesButton) Button mSavedUniversitiesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +47,27 @@ public class MainActivity extends AppCompatActivity {
                 .getReference()
                 .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
 
+mSearchedCountryReferenceListener = mSearchedCountryReference.addValueEventListener(new  ValueEventListener() {
+    @Override
+    public void onDataChange(@NonNull  DataSnapshot datasnapshot) {
+        for(DataSnapshot countrySnapshot :datasnapshot.getChildren()){
+            String country = countrySnapshot.getValue().toString();
+            Log.d("Country update","country" + country);
+        }
+
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+    }
+});
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mSavedUniversitiesButton.setOnClickListener(this);
 
 //
     }
@@ -58,6 +86,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void saveCountryToFirebase(String country) {
         mSearchedCountryReference.push().setValue(country);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v==mSavedUniversitiesButton) {
+            Intent intent = new Intent(MainActivity.this,SavedUniversitiesListActivity.class);
+            startActivity(intent);
+        }
+
     }
 
 
--- app/src/main/java/com/moringaschool/g_samaritan/SavedUniversitiesListActivity.java
+++ app/src/main/java/com/moringaschool/g_samaritan/SavedUniversitiesListActivity.java
@@ -1,13 +1,95 @@
 package com.moringaschool.g_samaritan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.moringaschool.g_samaritan.models.Universities;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SavedUniversitiesListActivity extends AppCompatActivity {
+    private DatabaseReference mUniversitiesReference;
+    private FirebaseRecyclerAdapter<Universities, FirebaseUniversityViewHolder> mFirebaseAdapter;
+
+    @BindView(R.id.samaritanRecyclerView)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.ErrorTextView)
+    TextView mTextView;
+    @BindView(R.id.progressBar)
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.samaritan_main);
+        ButterKnife.bind(this);
+
+        mUniversitiesReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_UNIVERSITIES);
+        setUpFirebaseAdapter();
+        hideProgressBar();
+        showRestaurants();
+    }
+
+
+    private void setUpFirebaseAdapter() {
+        FirebaseRecyclerOptions<Universities> options = new FirebaseRecyclerOptions.Builder<Universities>()
+                .setQuery(mUniversitiesReference, Universities.class)
+                .build();
+
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Universities, FirebaseUniversityViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebaseUniversityViewHolder firebaseUniversityViewHolder, int position, @NonNull Universities model) {
+                firebaseUniversityViewHolder.bindUniversity(model);
+
+            }
+
+            @NonNull
+            @Override
+            public FirebaseUniversityViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+                return new FirebaseUniversityViewHolder(view);
+
+            }
+        };
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mFirebaseAdapter!=null){
+            mFirebaseAdapter.stopListening();
+        }
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
+    }
+
+    private void showRestaurants() {
+        mRecyclerView.setVisibility(View.VISIBLE);
     }
 }

29737e396395995b4407497bf6c5a0fee1fa7dd0
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-07-21 20:49:10 +0300

    code implementation

--- app/src/main/java/com/moringaschool/g_samaritan/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/g_samaritan/CreateAccountActivity.java
@@ -2,13 +2,74 @@ package com.moringaschool.g_samaritan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    public  static final String TAG =CreateAccountActivity.class.getSimpleName();
+
+    private FirebaseAuth mAuth;
+    @BindView(R.id.createUserButton)
+    Button mCreateUserButton;
+    @BindView(R.id.nameEditText)
+    EditText mNameEditText;
+    @BindView(R.id.passwordEditText)
+    EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText)
+    EditText mConfirmPasswordEditText;
+    @BindView(R.id.loginTextView)
+    TextView mLoginTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        mLoginTextView.setOnClickListener(this);
+        mCreateUserButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+        intent.setFlags(FLAG_ACTIVITY_NEW_TASK | FLAG_ACTIVITY_NEW_TASK );
+        startActivity(intent);
+        finish();
+    }
+
+    private void createNewUser() {
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "Authentication successful");
+                    } else {
+                        Toast.makeText(CreateAccountActivity.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                };
     }
 }

5547a5f4f88bcb4b2f8262b96386eb678fa14103
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-07-21 18:36:46 +0300

    create layout

---
+++ app/src/main/java/com/moringaschool/g_samaritan/LOginActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.g_samaritan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LOginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

773bfcde1199c1b8d3e720a6a35bbdddf548d901
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-07-22 13:38:25 +0300

    addingxml

--- app/src/main/java/com/moringaschool/g_samaritan/MainActivity.java
+++ app/src/main/java/com/moringaschool/g_samaritan/MainActivity.java
@@ -3,6 +3,9 @@ package com.moringaschool.g_samaritan;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +20,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -90,6 +94,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 //    private void saveCountryToFirebase(String country) {
 //        mSearchedCountryReference.push().setValue(country);
+    }
+
+
+    @Override
+    public  boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
 

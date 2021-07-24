5414c2abbab3115a69707c3a787d17fabc5d63ca
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-07-24 11:21:19 +0300

    implementation

--- app/src/main/java/com/moringaschool/g_samaritan/Util/ItemTouchHelperAdapter.java
+++ app/src/main/java/com/moringaschool/g_samaritan/Util/ItemTouchHelperAdapter.java
@@ -1,4 +1,6 @@
 package com.moringaschool.g_samaritan.Util;
 
 public interface ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void  onItemDismiss(int position);
 }

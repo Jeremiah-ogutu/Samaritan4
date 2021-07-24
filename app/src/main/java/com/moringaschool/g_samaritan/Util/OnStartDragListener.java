f88728df9681ebc46e781b8782bfb05e3ad9115e
Jeremiah-ogutu <jeremiah.ogutu@student.moringaschool.com>
2021-07-24 11:35:05 +0300

    new class

--- app/src/main/java/com/moringaschool/g_samaritan/Util/OnStartDragListener.java
+++ app/src/main/java/com/moringaschool/g_samaritan/Util/OnStartDragListener.java
@@ -1,4 +1,7 @@
 package com.moringaschool.g_samaritan.Util;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
 }
